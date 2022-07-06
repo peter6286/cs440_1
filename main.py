a08b8c387d325d5eafe91e41edb2abf22de2f43f
peter.mac <953654375@qq.com>
2022-07-06 17:18:43 -0400

    17:30

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
-maze=[[0,0,0],
-      [0,1,0],
-      [0,0,0]]
+def shortestPathBinaryMatrix(grid):
+    row = len(grid)
+    col = len(grid[0])
 
+    if row == 1 and col == 1 and grid[0][0] == 0:
+        return 1
 
+    if grid[0][0] == 1 or grid[-1][-1] == 1:
+        return -1
 
+    step = 1
+    # grid[0][0] = 1
+    queue = [(0, 0)]
+    while queue:
+        current_len = len(queue)
+        for _ in range(current_len):
+            i, j = queue.pop(0)
+            grid[i][j] = 1
+            for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                temp_i = i + x
+                temp_j = j + y
+                if 0 <= temp_i < row and 0 <= temp_j < col:
+                    if temp_i == row - 1 and temp_j == col - 1:
+                        return step + 1
+                    if grid[temp_i][temp_j] == 1:
+                        continue
+                    if grid[temp_i][temp_j] == 0:
+                        grid[temp_i][temp_j] = 1
+                        queue.append((temp_i, temp_j))
+        step += 1
+    return -1
 
-def valid(maze,x,y):
-  if (x>=0 and x<len(maze) and y>=0 and y<len(maze[0]) and maze[x][y]==1):
-    return True
-  else:
-    return False
-# 移步函数实现
-def walk(maze,x,y):
-  # 如果位置是迷宫的出口，说明成功走出迷宫
-  if(x==0 and y==0):
-    print("successful!")
-    return True
-  # 递归主体实现
-  if valid(maze,x,y):
-    # print(x,y)
-    maze[x][y]=2 # 做标记，防止折回
-    # 针对四个方向依次试探，如果失败，撤销一步
-    if not walk(maze,x-1,y):
-      maze[x][y]=1
-    elif not walk(maze,x,y-1):
-      maze[x][y]=1
-    elif not walk(maze,x+1,y):
-      maze[x][y]=1
-    elif not walk(maze,x,y+1):
-      maze[x][y]=1
-    else:
-      return False # 无路可走说明，没有解
-  return True
-print(walk(maze,1,1))
+A = [[0, 0, 0],
+     [1, 1, 0],
+     [0, 0, 0]]
+
+print(shortestPathBinaryMatrix(A))
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
+import copy
 import random
+path=[]
+dirs=[(0,1),(1,0),(0,-1),(-1,0)]
+
 
 def rand_pick():
     value_list = [0, 1]
@@ -10,69 +14,77 @@ def rand_pick():
         if x < cumprob:
             break
     return item
-def UniquePathHelper(i, j, r, c, A):
-    # boundary condition or constraints
-    if (i == r or j == c):
-        return 0
-    if (A[i][j] == 1):
-        return 0
-    # base case
-    if (i == r - 1 and j == c - 1):
-        return 1
-    return UniquePathHelper(i + 1, j, r, c, A) + UniquePathHelper(i, j + 1, r, c, A)
+
+def passable(maze,pos): #检查迷宫maze的位置pos是否可通行
+    if 0 <= pos[0] < len(maze) and 0 <= pos[1] < len(maze[0]):
+        return maze[pos[0]][pos[1]]==0
+    return False
+
+#def mark(temp, pos):  # 给迷宫maze的位置pos标"2"表示“倒过了”
+#    temp[pos[0]][pos[1]] = 2
+
+def maze_solve(maze, start, end):
+    path.append(start)
+    if start == end:
+        return True
+    qu = []
+    visted = set()
+    visted.add(start)
+    #mark(temp, start)
+    qu.append(start)  # start位置入队
+    while qu:  # 还有候选位置
+        pos = qu.pop(0)  # 取出下一位置
+        for i in range(4):  # 检查每个方向
+            nextp = pos[0] + dirs[i][0], pos[1] + dirs[i][1]
+            if nextp not in visted and passable(maze, nextp):  # 找到新的探索方向
+                if nextp == end:  # 是出口，成功
+                    path.append(end)
+                    #print(maze)
+                    return True
+                #mark(temp, nextp)
+                visted.add(nextp)
+                qu.append(nextp)  # 新位置入队
+                path.append(nextp)
+    return False
 
 
 def check(matrix):
     row, col = len(matrix), len(matrix[0])
     low, high = 0, row * col - 1
     mid = (low + high) // 2
-    num = matrix[mid // col][mid % col]
-    UniquePathHelper(mid // col,mid % col, r, c, matrix)
+    midgrid = (mid//col,mid%col)
+    topleft = (0,0)
+    topright = (0,len(matrix)-1)
+    bottomleft = (len(matrix[0])-1,0)
+    bottomrihgt = (len(matrix[0])-1,len(matrix)-1)
 
-def UniquePathHelper(i, j, r, c, A):
-    # boundary condition or constraints
-    if (i == r or j == c):
-        return 0
-    if (A[i][j] == 1):
-        return 0
-    # base case
-    if (i == r - 1 and j == c - 1):
-        return 1
-    return UniquePathHelper(i + 1, j, r, c, A) + UniquePathHelper(i, j + 1, r, c, A)
-
-
-def uniquePathsWithObstacles(A):
-    r, c = len(A), len(A[0])
-
-    return UniquePathHelper(0, 0, r, c, A)
-
-
-
-rows = 3
-cols = 3
-maze=[[0 for i in range(cols)] for i in range(rows)]
-for i in range(rows):
-    for j in range(cols):
-        if rand_pick()==1:
-            maze[i][j] = 1
-        else:
-            maze[i][j] = 0
-
-print(maze)
-print(check(maze))
+    if maze_solve(matrix,midgrid,topleft) and maze_solve(matrix,topleft,bottomrihgt) \
+    and maze_solve(matrix,midgrid,topright) and maze_solve(matrix,midgrid,bottomleft) \
+    and maze_solve(matrix,midgrid,bottomrihgt):
+        return True
+    else:
+        return False
 
 
 
 
+def genratemaze(rows,cols):
+    maze=[[0 for i in range(cols)] for i in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            if rand_pick()==1:
+                maze[i][j] = 1
+            else:
+                maze[i][j] = 0
+    return maze
+
+A = [[0, 0, 0],
+     [1, 1, 0],
+     [0, 0, 0]]
+
+mymaze = genratemaze(9,9)
+#print(mymaze)
+print(check(A))
 
 
-def rand_pick():
-    value_list = [0, 1]
-    x = random.uniform(0 ,1)
-    probabilities = [0.3, 0.7]
-    cumprob = 0.0
-    for item , item_pro in zip(seq , probabilities):
-        cumprob += item_pro
-        if x < cumprob:
-            break
-    return item
+#check(A)
